1d5ee76d2b9d2e9f4977dc4b1599caaff7a6ea5e
thekcr85 <thekcr85@gmail.com>
2025-12-20 18:13:36 +0100

    feat(middleware): add MyCustomMiddleware implementing IMiddleware

---
+++ LearnMiddleware/Middleware/MyCustomMiddleware.cs
@@ -0,0 +1,14 @@
+﻿
+namespace LearnMiddleware.Middleware;
+
+public class MyCustomMiddleware : IMiddleware
+{
+	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+	{
+		await context.Response.WriteAsync("MyCustomMiddleware: Before calling next\n");
+
+		await next(context);
+
+		await context.Response.WriteAsync("MyCustomMiddleware: After calling next\n");
+	}
+}
