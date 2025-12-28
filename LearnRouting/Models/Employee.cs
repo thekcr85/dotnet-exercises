f0d309b868191d948889d45e5a6bc63d52453a13
thekcr85 <thekcr85@gmail.com>
2025-12-28 11:02:29 +0100

    feat: add Employee domain model

---
+++ LearnRouting/Models/Employee.cs
@@ -0,0 +1,15 @@
+public class Employee
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Position { get; set; }
+	public double Salary { get; set; }
+
+	public Employee(int id, string name, string position, double salary)
+	{
+		Id = id;
+		Name = name;
+		Position = position;
+		Salary = salary;
+	}
+}
